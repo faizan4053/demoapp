2048d4838586bd695ce72d56ace99ffcdd7b399e
faizan4053 <mohdf078@gmail.com>
2021-06-07 14:50:13 +0530

    welcome java project

---
+++ DEMOAPP/src/demoapp/Welcome.java
@@ -0,0 +1,10 @@
+package demoapp;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to java project");
+	}
+
+}
